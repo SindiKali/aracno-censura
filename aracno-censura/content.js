effcb9e3206393340e9b73a3454b6d61b924fc71
SindiKali <m4ttpizz4@gmail.com>
2025-09-30 20:49:37 -0300

    Add files via upload

---
+++ aracno-censura/content.js
@@ -0,0 +1,22 @@
+console.log("Extensão Anti-Aranha Ativa!");
+
+const keywords = ["aranha", "spider", "tarantula", "aracnídeo", "arachnid", "salticidae", "thomisidae", "lycosidae", "theraphosidae", "araneae", "araneus diadematus", "phoneutria", "theraphosidae", "atracinae", "latrodectus", "heteropoda", "solifugae", "theraphosa blondi", "theraphosa"];
+
+function censorImages() {
+    const images = document.querySelectorAll('img');
+
+    images.forEach(img => {
+        const altText = img.alt.toLowerCase();
+        const srcText = img.src.toLowerCase();
+
+        const shouldCensor = keywords.some(keyword => altText.includes(keyword) || srcText.includes(keyword));
+
+        if (shouldCensor) {
+            console.log("Aranha encontrada! Censurando imagem:", img.src);
+            img.style.filter = "blur(30px)";
+            img.style.border = "3px solid red";
+        }
+    });
+}
+
+censorImages();
